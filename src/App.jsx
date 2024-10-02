85c4a69b8cf1f0967bf3d69c81aa7d24659d3b8a
kubilay taşkafa <info.kubilay0@gmail.com>
2024-10-02 10:31:57 +0300

    Project Create

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/RouterConfig.jsx
@@ -0,0 +1,5 @@
+const RouterConfig = () => {
+  return <div></div>;
+};
+
+export default RouterConfig;
